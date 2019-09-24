634da1b35c2c6ceb0bd116a6622f695ee025b233
Leo <leo.vornberger@gmx.de>
2019-09-24 23:06:28 +0200

    Initial Commit

---
+++ BilderDrehen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BilderDrehen
+//
+//  Created by Leo Vornberger on 24.09.19.
+//  Copyright © 2019 Leo Vornberger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
